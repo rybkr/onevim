083a9f3d23099299a709d0636400e26a07ed7954
Ryan Baker <rbaker@xtier.com>
2025-10-17 22:26:14 -0400

    Add misc plugins

---
+++ lua/onevim/plugins/misc.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup()
+        end,
+    },
+    {
+        "numToStr/Comment.nvim",
+        keys = {
+            { "gcc", mode = "n" },
+            { "gc", mode = { "n", "v" } },
+        },
+        config = function()
+            require("Comment").setup()
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = "│" },
+                    change = { text = "│" },
+                    delete = { text = "_" },
+                },
+            })
+        end,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("which-key").setup()
+        end,
+    },
+}
