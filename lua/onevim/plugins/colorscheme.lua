1cff678d7e9a8b364a3b3a2dc534edf57465b01d
Ryan Baker <rbaker@xtier.com>
2025-11-06 13:58:15 -0500

    Add nightfly colors

--- lua/onevim/plugins/colorscheme.lua
+++ lua/onevim/plugins/colorscheme.lua
@@ -1,16 +1,24 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("tokyonight").setup({
-            style = "night",
-            transparent = false,
-            terminal_colors = true,
-            styles = { 
-                comments = { italic = true },
-                keywords = { italic = true },
-            },
-        })
-    end,
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                transparent = false,
+                terminal_colors = true,
+                styles = { 
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                },
+            })
+        end,
+    },
+    {
+        "bluz71/vim-nightfly-colors",
+        name = "nightfly",
+        lazy = false,
+        priority = 1000
+    },
 }
